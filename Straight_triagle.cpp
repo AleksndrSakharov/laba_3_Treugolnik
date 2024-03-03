3f7c330f54bb9c3535af4ea8a189a5689b6153ec
AleksndrSakharov <mr.11aleksandrsasharov11@mail.ru>
2024-03-03 20:46:03 +0300

    43

--- Straight_triagle.cpp
+++ Straight_triagle.cpp
@@ -1,1 +1,28 @@
 #include "Straight_triagle.h"
+
+void Straight_triagle::CheckTriagle(){
+    _a = pow(_x1-_x2, 2) + pow(_y1-_y2, 2);
+    _b = pow(_x2-_x3, 2) + pow(_y2-_y3, 2);
+    _c = pow(_x1-_x3, 2) + pow(_y1-_y3, 2);
+    if ((_c == _b && _a == _b) || _c == 0 || _a == 0 || _b == 0) throw "This is not a straight triagle";
+    if ((_b == _a + _c) || (_a == _c + _b) || (_c == _a + _b)){
+        std::cout << "This is a straight triagle" << std::endl;
+    }
+    else throw "This is not a straight triagle";
+}
+
+double Straight_triagle::countArea(){
+    Straight_triagle::CheckTriagle();
+    if (_c > _a && _c > _b) return pow(_a * _b, 0.5) / 2;
+    if (_b > _a && _b > _c) return pow(_a * _c, 0.5) / 2;
+    if (_a > _c && _a > _b) return pow(_c * _b, 0.5) / 2;
+}
+
+Straight_triagle::Straight_triagle(const Straight_triagle &tmp){//Копирование
+    _x1 = tmp._x1;
+    _x2 = tmp._x2;
+    _x3 = tmp._x3;
+    _y1 = tmp._y1;
+    _y2 = tmp._y2;
+    _y3 = tmp._y3;
+}
--- Straight_triagle.h
+++ Straight_triagle.h
@@ -5,16 +5,81 @@
 class Straight_triagle
 {
 private:
-    double _x1, _x2, __x3, _y1, _y2, _y3;
+    double _x1, _x2, _x3, _y1, _y2, _y3;
     double _a, _b, _c;
 public:
-    Straight_triagle(double x1, double x2,  double x3, double y1, double y2, double y3){
-        _a = pow(_x1-_x2, 2) + pow(y1-y2, 2);
-        _b = pow(x2-x3, 2) + pow(y2-y3, 2);
-        _c = pow(x1-x3, 2) + pow(y1-y3, 2);
-        if (pow(_c, 2) != pow(_a, 2) + pow(_b, 2)){
-            throw "NO";
-        } 
+    Straight_triagle(){//По умолчанию
+        _x1 = 1;
+        _x2 = 0;
+        _x3 = 0;
+        _y1 = 0;
+        _y2 = 1;
+        _y3 = 0;
     };
+
+    Straight_triagle(double x1, double x2, double x3, double y1, double y2, double y3){//С параметрами
+        _x1 = x1;
+        _x2 = x2;
+        _x3 = x3;
+        _y1 = y1;
+        _y2 = y2;
+        _y3 = y3;
+        CheckTriagle();
+    };
+
+    Straight_triagle(const Straight_triagle &tmp);//Копирование
+
+    void CheckTriagle();
+
+    double countArea();
     
+    inline double getX1(){return _x1;};
+    inline double getX2(){return _x2;};
+    inline double getX3(){return _x3;};
+    inline double getY1(){return _y1;};
+    inline double getY2(){return _y2;};
+    inline double getY3(){return _y3;};
+
+    void setX1(double x1){_x1 = x1;};
+    void setX2(double x2){_x2 = x2;};
+    void setX3(double x3){_x3 = x3;};
+    void setY1(double y1){_y1 = y1;};
+    void setY2(double y2){_y2 = y2;};
+    void setY3(double y3){_y3 = y3;};
+
+
+
+    Straight_triagle& operator=(const Straight_triagle& tmp){//Присваивание
+        _x1 = tmp._x1;
+        _x2 = tmp._x2;
+        _x3 = tmp._x3;
+        _y1 = tmp._y1;
+        _y2 = tmp._y2;
+        _y3 = tmp._y3;
+        return *this;
+    }
+
+
+
+
+
+
+    friend std::ostream& operator << (std::ostream &os, const Straight_triagle &straight_triagle){
+        return os << "A = (" << straight_triagle._x1 << ";" << straight_triagle._y1 << "), B = (" << straight_triagle._x2 << ";" << straight_triagle._y2 << "), C = (" << straight_triagle._x3 << ";" << straight_triagle._y3 << ")";
+    }
+
+    friend std::istream& operator >> (std::istream& in, Straight_triagle &straight_triagle){
+        double x1, x2, x3, y1, y2, y3;
+        in >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+        straight_triagle.setX1(x1);
+        straight_triagle.setX2(x2);
+        straight_triagle.setX3(x3);
+        straight_triagle.setY1(y1);
+        straight_triagle.setY2(y2);
+        straight_triagle.setY3(y3);
+        return in;
+    }
+
+
+    ~Straight_triagle(){}
 };
